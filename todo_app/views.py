8f0956bad74399c8610d3e813d539ed95b79b76c
Amelia Risner <amelia.risner0@gmail.com>
2023-01-27 14:46:39 -0800

    ughhh buggy HTML is an actual nightmare

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDoList
 
-# Create your views here.
+
+class ListListView(ListView):
+    #ToDoList clast created in models.py
+    model = ToDoList
+    template_name = "todo_app/index.html"
+
+
