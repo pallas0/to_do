e310a14813bd16b310b874d520af1b5ea74e9317
Amelia Risner <amelia.risner0@gmail.com>
2023-01-30 13:00:18 -0800

    list and item delete templates created

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from django.views.generic import (
     ListView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
 from .models import ToDoList, ToDoItem
 
@@ -79,4 +80,20 @@ class ItemUpdate(UpdateView):
 
     def get_success_url(self):
         return reverse("list", args=[self.object.todo_list_id])
+
+class ListDelete(DeleteView):
+    model = ToDoList
+    #using reverse_lazy instead bc urls are not loaded when the file is imported
+    success_url = reverse_lazy("index")
+
+class ItemDelete(DeleteView):
+    model = ToDoItem
+
+    def get_success_url(self):
+        return reverse_lazy("list", args=[self.kwargs["list_id"]])
+
+    def  get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["todo_list"] = self.object.todo_list
+        return context
 
