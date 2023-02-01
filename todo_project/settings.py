314ab8cee31ff9b1da4d9673962d1a249ca71df6
Amelia Risner <amelia.risner0@gmail.com>
2023-02-01 11:53:51 -0800

    updated allowed host and csrf settings

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-#ALLOWED_HOSTS = ['web-production-a753.up.railway.app', '127.0.0.1']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-a753.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['web-production-a753.up.railway.app/']
 
 
 # Application definition
