ec4b983ae26420f04d00a3ffb7a8139f08358512
Amelia Risner <amelia.risner0@gmail.com>
2023-02-04 14:56:35 -0600

    url in allowed_hosts modified

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = False
 COLLECTSTATIC_DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-a753.up.railway.app', '127.0.0.1', '127.0.0.1:8000']
-CSRF_TRUSTED_ORIGINS = ['web-production-a753.up.railway.app/', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['todo-production-7e65.up.railway.app/', '127.0.0.1', '127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['todo-production-7e65.up.railway.app/', '127.0.0.1:8000']
 
 # Application definition
 
